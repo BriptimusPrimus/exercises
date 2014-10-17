199d24056b526639b5b453665e3d60cdb9f6062b
briptimus <brituor@gmail.com>
2014-10-17 11:25:15 -0700

    added project EmployeeApp

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Field data.
+        private string empName;
+        private int empID;
+        private float currPay;
+        private int empAge;
+
+        // Constructors.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+            : this(name, 0, id, pay) { }
+        public Employee(string name, int age, int id, float pay)
+        {
+            Name = name;
+            Age = age;
+            ID = id;
+            Pay = pay;
+        }
+
+        // Methods.
+        public void GiveBonus(float amount)
+        { Pay += amount; }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine("Name: {0}", Name);
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Age: {0}", Age);
+            Console.WriteLine("Pay: {0}", Pay);
+        }
+
+        // Properties!
+        public string Name
+        {
+            get { return empName; }
+            set
+            {
+                if (value.Length > 15)
+                    Console.WriteLine("Error!  Name must be less than 16 characters!");
+                else
+                    empName = value;
+            }
+        }
+
+        // We could add additional business rules to the sets of these properties;
+        // however, there is no need to do so for this example.
+        public int ID
+        {
+            get { return empID; }
+            set { empID = value; }
+        }
+        public float Pay
+        {
+            get { return currPay; }
+            set { currPay = value; }
+        }
+
+        public int Age
+        {
+            get { return empAge; }
+            set { empAge = value; }
+        }
+
+    }
+}
