429d24fe9c4c35ca2b9a6e4d35a9777d15e2ddac
briptimus <brituor@gmail.com>
2014-10-17 15:43:37 -0700

    new project ConstData added

---
+++ ConstData/MyMathClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class MyMathClass
+    {
+        // Read-only fields can be assigned in ctors,
+        // but nowhere else.
+        public static readonly double PI;
+
+        static MyMathClass()
+        {
+            PI = 3.14;
+        }
+
+        //// Error!
+        //public void ChangePI()
+        //{ PI = 3.14444; }
+    }
+}
