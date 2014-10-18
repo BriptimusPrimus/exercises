0d39288fea4847ae238e34af343892a9a3ecc581
briptimus <brituor@gmail.com>
2014-10-17 17:40:23 -0700

    added new project BasicInheritance

---
+++ BasicInheritance/MiniVan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    // MiniVan derives from Car.
+    class MiniVan : Car
+    {
+        public void TestMethod()
+        {
+            // OK! Can access public members
+            // of a parent within a derived type.
+            Speed = 10;
+
+            // Error! Cannot access private
+            // members of parent within a derived type.
+            // currSpeed = 10;
+        }
+    } 
+}
