674de2be494a2c1804b4584c57e1ab0e19ae52c6
briptimus <brituor@gmail.com>
2014-10-18 17:44:03 -0700

    added new project Shapes

---
+++ Shapes/Shape.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    // The abstract base class of the hierarchy.
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+
+        public string PetName { get; set; }
+
+        // Force all child classes to define how to be rendered.
+        public abstract void Draw();
+    }
+
+    // If we did not implement the abstract Draw() method, Circle would also be
+    // considered abstract, and would have to be marked abstract!
+    class Circle : Shape
+    {
+        public Circle() { }
+        public Circle(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Circle", PetName);
+        }
+    }
+
+    // Hexagon DOES override Draw().
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine("Drawing {0} the Hexagon", PetName);
+        }
+    }
+}
