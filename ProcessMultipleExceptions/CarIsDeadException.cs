61dca456c3f2ec807712325d298c641faff8fa2d
briptimus <brituor@gmail.com>
2014-10-19 14:58:42 -0700

    added new project ProcessMultipleExceptions

---
+++ ProcessMultipleExceptions/CarIsDeadException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExceptions
+{
+    [Serializable]
+    public class CarIsDeadException : ApplicationException
+    {
+        public CarIsDeadException() { }
+        public CarIsDeadException(string message) : base(message) { }
+        public CarIsDeadException(string message, Exception inner) : base(message, inner) { }
+        protected CarIsDeadException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+
+        // Any additional custom properties, constructors and data members...
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        // Feed message to parent constructor.
+        public CarIsDeadException(string message, string cause, DateTime time)
+            : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+    }
+}
