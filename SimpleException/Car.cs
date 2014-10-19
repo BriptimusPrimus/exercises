e933ee95eedb1e52584752eed81ccedc87f7a801
briptimus <brituor@gmail.com>
2014-10-19 11:57:56 -0700

    more logic to exception handling

--- SimpleException/Car.cs
+++ SimpleException/Car.cs
@@ -48,8 +48,17 @@ namespace SimpleException
                     carIsDead = true;
                     CurrentSpeed = 0;
 
-                    // Use the "throw" keyword to raise an exception.
-                    throw new Exception(string.Format("{0} has overheated!", PetName));
+                    // We need to call the HelpLink property, thus we need
+                    // to create a local variable before throwing the Exception object.
+                    Exception ex =
+                      new Exception(string.Format("{0} has overheated!", PetName));
+                    ex.HelpLink = "http://www.CarsRUs.com";
+
+                    // Stuff in custom data regarding the error.
+                    ex.Data.Add("TimeStamp",
+                      string.Format("The car exploded at {0}", DateTime.Now));
+                    ex.Data.Add("Cause", "You have a lead foot.");
+                    throw ex;
                 }
                 else
                     Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using System.Collections;
+
 namespace SimpleException
 {
     class Program
@@ -26,9 +28,26 @@ namespace SimpleException
             catch (Exception e)
             {
                 Console.WriteLine("\n*** Error! ***");
-                Console.WriteLine("Method: {0}", e.TargetSite);
+
+                Console.WriteLine("Member name: {0}", e.TargetSite);
+                Console.WriteLine("Class defining member: {0}", 
+                    e.TargetSite.DeclaringType);
+                Console.WriteLine("Member type: {0}", e.TargetSite.MemberType);
+
                 Console.WriteLine("Message: {0}", e.Message);
                 Console.WriteLine("Source: {0}", e.Source);
+
+                Console.WriteLine("Stack: {0}", e.StackTrace);
+
+                Console.WriteLine("Help Link: {0}", e.HelpLink);
+
+                // By default, the data field is empty, so check for null.
+                Console.WriteLine("\n-> Custom Data:");
+                if (e.Data != null)
+                {
+                    foreach (DictionaryEntry de in e.Data)
+                        Console.WriteLine("-> {0}: {1}", de.Key, de.Value);
+                }
             }
 
             // The error has been handled, processing continues with the next statement.
