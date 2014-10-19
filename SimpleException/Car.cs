32c0ad9747599cf4dacc2856d86d5595bfb9ddf4
briptimus <brituor@gmail.com>
2014-10-19 11:05:27 -0700

    added new project SimpleException

---
+++ SimpleException/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Constant for maximum speed.
+        public const int MaxSpeed = 100;
+
+        // Car properties.
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car still operational?
+        private bool carIsDead;
+
+        // A car has-a radio.
+        private Radio theMusicBox = new Radio();
+
+        // Constructors.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Delegate request to inner object.
+            theMusicBox.TurnOn(state);
+        }
+
+        // This time, throw an exception if the user speeds up beyond MaxSpeed.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead)
+                Console.WriteLine("{0} is out of order...", PetName);
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed >= MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+
+                    // Use the "throw" keyword to raise an exception.
+                    throw new Exception(string.Format("{0} has overheated!", PetName));
+                }
+                else
+                    Console.WriteLine("=> CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
