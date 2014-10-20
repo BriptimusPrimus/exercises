555b6c5f22a9398a176823ad527d913616f5b2d3
briptimus <brituor@gmail.com>
2014-10-20 09:46:33 -0700

    logic added

--- MIInterfaceHierarchy/Rectangle.cs
+++ MIInterfaceHierarchy/Rectangle.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace MIInterfaceHierarchy
 {
-    class Rectangle
+    class Rectangle : IShape
     {
+        public int GetNumberOfSides()
+        { return 4; }
+
+        public void Draw()
+        { Console.WriteLine("Drawing..."); }
+
+        public void Print()
+        { Console.WriteLine("Prining..."); }
     }
 }
--- MIInterfaceHierarchy/Square.cs
+++ MIInterfaceHierarchy/Square.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace MIInterfaceHierarchy
 {
-    class Square
+    class Square : IShape
     {
-    }
+        // Using explicit implementation to handle member name clash.
+        void IPrintable.Draw()
+        {
+            // Draw to printer ...
+        }
+        void IDrawable.Draw()
+        {
+            // Draw to screen ...
+        }
+        public void Print()
+        {
+            // Print ...
+        }
+        public int GetNumberOfSides()
+        { return 4; }
+    } 
 }
