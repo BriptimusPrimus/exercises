b42c4ce07518fb83fcd3c49fa9dc48e0096a4bab
briptimus <brituor@gmail.com>
2014-10-19 22:52:41 -0700

    added new project CustomInterface

---
+++ CustomInterface/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomInterface
+{
+    // The abstract base class of the hierarchy.
+    abstract class Shape
+    {
+        public Shape(string name = "NoName")
+        { PetName = name; }
+
+        public string PetName { get; set; }
+
+        // Force all child classes to define how to be rendered.
+        public abstract void Draw();
+    }
+}
