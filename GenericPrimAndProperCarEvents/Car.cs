64d00a04e18b35a77105268d3b53f60bc407486a
briptimus <brituor@gmail.com>
2014-10-21 22:24:43 -0700

    added new project GenericPrimAndProperCarEvents

---
+++ GenericPrimAndProperCarEvents/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrimAndProperCarEvents
+{
+    public class CarEventArgs : EventArgs
+    {
+        public readonly string msg;
+        public CarEventArgs(string message)
+        {
+            msg = message;
+        }
+    }
+
+    public class Car
+    {
+        // This car can send these events.
+        public event EventHandler<CarEventArgs> Exploded;
+        public event EventHandler<CarEventArgs> AboutToBlow;
+
+        // Internal state data.
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+
+        // Is the car alive or dead?
+        private bool carIsDead;
+
+        // Class constructors.
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+        
+        public void Accelerate(int delta)
+        {
+            // If the car is dead, fire Exploded event.
+            if (carIsDead)
+            {
+                if (Exploded != null)
+                    Exploded(this, new CarEventArgs("Sorry, this car is dead..."));
+            }
+            else
+            {
+                CurrentSpeed += delta;
+
+                // Almost dead?
+                if (10 == MaxSpeed - CurrentSpeed
+                  && AboutToBlow != null)
+                {
+                    AboutToBlow(this, new CarEventArgs("Careful buddy!  Gonna blow!"));
+                }
+
+                // Still OK!
+                if (CurrentSpeed >= MaxSpeed)
+                    carIsDead = true;
+                else
+                    Console.WriteLine("CurrentSpeed = {0}", CurrentSpeed);
+            }
+        }
+    }
+}
---
+++ GenericPrimAndProperCarEvents/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPrimAndProperCarEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** Prim and Proper Events *****\n");
+
+            // Make a car as usual.
+            Car c1 = new Car("SlugBug", 100, 10);
+
+            // Register event handlers.
+            c1.AboutToBlow += CarAboutToBlow;
+
+            Console.WriteLine("***** Speeding up *****");
+            for (int i = 0; i < 6; i++)
+                c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        public static void CarAboutToBlow(object sender, CarEventArgs e)
+        {
+            // Just to be safe, perform a
+            // runtime check before casting.
+            if (sender is Car)
+            {
+                Car c = (Car)sender;
+                Console.WriteLine("Critical Message from {0}: {1}", c.PetName, e.msg);
+            }
+        }
+    }
+}
