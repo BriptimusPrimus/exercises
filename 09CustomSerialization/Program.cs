97d82cb2d381c08db9327232bb38be86f4b55d59
briptimus <brituor@gmail.com>
2014-10-28 22:14:09 -0800

    added a serializable class using serialization-centric attributes to project 09CustomSerialization

--- 09CustomSerialization/Program.cs
+++ 09CustomSerialization/Program.cs
@@ -52,4 +52,27 @@ namespace _09CustomSerialization
         }
     }
 
+    [Serializable]
+    class MoreData
+    {
+        private string dataItemOne = "First data block";
+        private string dataItemTwo = "More data";
+
+        [OnSerializing]
+        private void OnSerializing(StreamingContext context)
+        {
+            // Called during the serialization process.
+            dataItemOne = dataItemOne.ToUpper();
+            dataItemTwo = dataItemTwo.ToUpper();
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            // Called when the deserialization process is complete.
+            dataItemOne = dataItemOne.ToLower();
+            dataItemTwo = dataItemTwo.ToLower();
+        }
+    }
+
 }
